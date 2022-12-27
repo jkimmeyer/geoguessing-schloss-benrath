4f9d2df1b0124996fc8f23c5bfce036206ea359f
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2022-12-27 13:20:00 +0100

    Appease i18n

--- config/i18n.ts
+++ config/i18n.ts
@@ -1,12 +1,10 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
-import landingPage from "../locales/de/landingPage";
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources: {
-      ...landingPage,
     },
     lng: "de",
     fallbackLng: "de",
