87a84db1580298fe4ed56506a81c4b4482e0b7b9
Tobias Eckert <eckert.tobias@gmail.com>
2023-02-26 14:50:23 +0100

    add SbImage Component

--- components/SbImage/SbImage.tsx
+++ components/SbImage/SbImage.tsx
@@ -1,22 +1,19 @@
-import imageStyles from './SbImage.module.css';
 import Image from 'next/image';
 
 interface Props {
   src: string,
-  alt?: string,
   width: number,
-  height: number
+  height: number,
+  alt: string,
 }
 
 const SbImage: React.FC<Props> = (props) => {
   return (
-    <div>
-      <Image 
-        className={imageStyles['sb-image']}
-        width={props.width}
-        height={props.height}
-        src={'https://res.cloudinary.com/dzp9ta4c7/image/upload/v1677414821/detailshots/object_01_window_03_trcz7n.jpg'} alt="Schloss Benrath von außen"></Image>
-    </div>
+    <Image 
+      width={props.width}
+      height={props.height}
+      src={props.src} 
+      alt={props.alt} />
   );
 }
 
