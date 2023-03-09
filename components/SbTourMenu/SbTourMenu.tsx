c8960dff2a3182666c011ae12e6db3ff20153992
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2023-03-09 15:42:28 +0100

    Add mute button

--- components/SbTourMenu/SbTourMenu.tsx
+++ components/SbTourMenu/SbTourMenu.tsx
@@ -1,21 +1,50 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { UserContext } from '../../context/userContext';
-import { UserContextType } from '../../types';
+import { IconNames, UserContextType } from '../../types';
 import SbButton from '../SbButton/SbButton';
+import SbIcon from '../SbIcon/SbIcon';
 import tourMenuStyles from './SbTourMenu.module.css'
 
 interface Props {
   toggleMenu: () => void;
 }
 
+declare global {
+  interface Window {
+    tour: {
+      play: () => void;
+      pause: () => void;
+    }
+  }
+}
+
 const SbTourMenu: React.FC<Props> = ({
   toggleMenu
 }) => {
+  const { currentUser } = useContext(UserContext) as UserContextType;
+  const [tone, setTone] = useState(true);
 
-  const { currentUser } = useContext(UserContext) as UserContextType
+  const toggleSound = () => {
+    setTone(current => !current)
+  }
+
+  useEffect(() => {
+    if (tone) {
+      window.tour.resume()
+    }
+    else {
+      window.tour.pause()
+    }
+  }, [tone])
 
   return (
     <div className={tourMenuStyles['tour-menu']}>
+      <SbButton icon={
+        <SbIcon icon={tone ? IconNames.Sound : IconNames.Mute} />
+      }
+        iconOnly={true}
+        onClick={() => toggleSound()} />
+
       <p>{`Hallo, ${currentUser?.playerName}!`}</p>
       <SbButton onClick={() => toggleMenu()}>Weiterspielen</SbButton>
 
