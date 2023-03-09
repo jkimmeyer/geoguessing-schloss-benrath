2d1426635de9e4cb4dc16686817dc4c4ea4275b6
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2023-03-09 11:33:37 +0100

    Image compression

---
+++ components/SbImage/SbImage.tsx
@@ -0,0 +1,36 @@
+import Image, { StaticImageData } from 'next/image'
+interface Props extends React.HTMLAttributes<HTMLDivElement> {
+    src: string | StaticImageData;
+    alt: string;
+  width: number;
+  height: number;
+}
+
+const SbImage: React.FC<Props> = ({
+    src,
+    alt,
+    width,
+    height,
+    className
+}) => {
+
+  const computedSrc = (src: string | StaticImageData) => {
+    if (typeof (src) !== 'string') {
+      return src
+    }
+
+    let computedSrc = src;
+
+    if (src.startsWith("https://res.cloudinary.com/")) {
+      computedSrc = computedSrc.replace('/upload', '/upload/w_1000/q_auto/f_auto')
+    }
+
+    return computedSrc
+  }
+
+  return (
+    <Image className={className} src={computedSrc(src)} alt={alt} width={width} height={height} />
+  )
+}
+
+export default SbImage;
