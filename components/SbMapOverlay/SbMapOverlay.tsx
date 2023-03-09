3fd78ace509048b1f4339ddd2ad532b83f7da9ce
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2023-03-09 16:22:31 +0100

    Close map on click

--- components/SbMapOverlay/SbMapOverlay.tsx
+++ components/SbMapOverlay/SbMapOverlay.tsx
@@ -7,27 +7,37 @@ import { publish } from "../../lib/events";
 export const SbMapOverlay = () => {
   const [mapOpen, setMapOpen] = useState(false);
 
+  const resume = (map: HTMLElement | null) => {
+    publish('game:resume', { noOverlay: true })
+
+    if (map) {
+      map.style.display = "none";
+    }
+
+    setMapOpen(false);
+  }
+
+  const pause = (map: HTMLElement | null) => {
+    publish('game:paused', { noOverlay: true })
+
+    if (map) {
+      map.style.display = "block";
+    }
+    setMapOpen(true);
+  }
+
   const toggleMap = () => {
     const map = document.getElementById("7");
 
     if (mapOpen) {
-      publish('game:resume', {noOverlay: true})
-
-      if (map) {
-        map.style.display = "none";
-      }
+      resume(map)
     } else {
-      publish('game:paused', { noOverlay: true })
-
-      if (map) {
-        map.style.display = "block";
-      }
+      map?.addEventListener("click", () => {
+        resume(map)
+      });
+      pause(map)
     }
-
-    setMapOpen(current => !current);
   }
-
-
 
   return (
     <div className={sbMenuFrameStyles['menu-frame--menu-container']} data-map>
