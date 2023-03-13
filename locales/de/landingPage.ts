c476ba6c4a07b0822e8a6391167e41dc9adad125
Johannes Kimmeyer <johannes.kimmeyer@zweitag.de>
2023-03-13 11:11:47 +0100

    Fix fonts and minor improvements

--- locales/de/landingPage.ts
+++ locales/de/landingPage.ts
@@ -2,19 +2,19 @@ const landingPage = {
   intro: {
     chapter1: {
       title: "Lerne das Schloss ganz neu kennen!",
-      teaserText: "Lerne das Schloss Benrath neu kennen. Begib dich auf eine 360°Blder Tour durch das historische Schloss. Lerne spielerisch etwas über die Austellungsstücke des Schlosses. Messe dich mit andern Spieler:innen, wer sich am besten im Schloss auskennt gewinnt!"
+      teaserText: "Lerne das Schloss Benrath neu kennen. Begib dich auf eine 360° Tour durch das historische Schloss. Lerne spielerisch etwas über die Austellungsstücke des Schlosses. Messe dich mit andern Spieler:innen und finde heraus, wer sich am Besten im Schloss auskennt!"
     },
     chapter2: {
       title: "Vom 17. Jahrhundert …",
-      teaserText: "Die Austellungsstücke die sich im Schloss befinden sind zeitgenössische Gegenstände. Jedes ein Kunstwerk für sich und ein Meilenstein in der Geschichte."
+      teaserText: "Das Lustschloss auf dem Lande. Zwölf Kilometer vor den früheren Toren der Stadt ließ Kurfürst Karl Theodor von der Pfalz 1756 bis 1773 von Nicolas de Pigage eine Maison de plaisance mit großzügigen Gartenanlagen errichten. Die Austellungsstücke im Schloss sind zeitgenössische Gegenstände. Jedes ist ein Kunstwerk für sich und ein Meilenstein in der Geschichte des Schlosses. Erfahre spannende Details und höre die Geschichten hinter den Gegenständen im Schloss."
     },
     chapter3: {
       title: "… bis in die Gegenwart",
-      teaserText: "Bis ins 20. Jahrhundert haben berühmte Persönlichkeiten ihre Spuren im Schloss hinterlassen. Starte die Tour und erfahre wer alles im Schloss war."
+      teaserText: "Bis ins 20. Jahrhundert haben berühmte Persönlichkeiten ihre Spuren im Schloss hinterlassen. Heutzutage bietet die Stiftung Schloss und Park Benrath die Möglichkeit, sich standesamtlich im Schloss trauen zu lassen. Starte die Tour und erfahre, welche Berühmtheiten das Schloss vor dieser Zeit mit ihrem Besuch beehrt haben."
     },
     chapter4: {
       title: "Interesse geweckt?",
-      teaserText: "Wie das Spiel funktioniert erklären wir die auf den nächsten Seiten. Probiere es einfach mal aus und lass dich überraschen."
+      teaserText: "Wie das Spiel funktioniert, erklären wir die auf den nächsten Seiten. Probiere es einfach mal aus und lass dich überraschen."
     }
   },
   startTour: "Tour starten",
